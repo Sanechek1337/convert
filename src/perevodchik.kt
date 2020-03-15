9ee7c1f8c5bc224900631000372252aa57a78809
sanechek1337 <rednaxela659@gmail.com>
2020-03-15 21:09:15 +0600

    Добавил переводчик через for

--- src/perevodchik.kt
+++ src/perevodchik.kt
@@ -1,38 +1,40 @@
+//Через while
+
 fun main() {
-    var example = "ИЯ-32"
-    var rWords = listOf(' ', '-', '(', ')', '1', '2', '3', '4', 'Д', 'О', 'В', 'И', 'Н', 'Ф', 'К', 'Р', 'П', 'М', 'С', 'Т', 'Я')
-    var eWords = listOf(' ', '-', '(', ')', '1', '2', '3', '4', 'd', 'o', 'v', 'i', 'n', 'f', 'k', 'r', 'p', 'm', 's', 'T', 'y', 'a')
-    var chStr = mutableListOf<String>()
+    var example = "Д-4 сс"
+    var rWords = listOf(' ', '-', '(', ')', '1', '2', '3', '4', 'Д', 'О', 'В', 'И', 'Н', 'Ф', 'К', 'Р', 'П', 'М', 'С', 'Т', 'с', 'Я')
+    var eWords = listOf(' ', '-', '(', ')', '1', '2', '3', '4', 'd', 'o', 'v', 'i', 'n', 'f', 'k', 'r', 'p', 'm', 's', 'T', 's', 'y', 'a')
+    var chStr = mutableListOf<Char>()
+    var finStr = " "
     var i = 0
 
    //Цикл прогоняет всю строку
    while (i < example.length) {
        var j = 0
-       println(">>> i = $i")
        //Проверка нужного символа на совпадения
        while (j < rWords.size) {
-           println(">>> j = $j")
            var box = example[i]
-           println("box = $box")
            if (rWords[j] == example[i]) {
-               if (j == 20) {
+               if (j == 21) {
                    box = eWords[j]
-                   chStr.add(i, box.toString())
+                   chStr.add(box)
                    box = eWords[j+1]
-                   chStr.add(i+1, box.toString())
-                   //i++
+                   chStr.add(box)
                    break
                }
                box = eWords[j]
-               chStr.add(i, box.toString())
+               chStr.add(box)
                break
            }
-           println("массив = $chStr")
            j++
-           println("<<< j = $j")
        }
        i++
-       println("<<< i = $i")
     }
+
+    for (i in chStr) {
+        finStr += i
+    }
+
     println("Измененная строка - $chStr")
+    println("Готовая строка - $finStr")
 }
